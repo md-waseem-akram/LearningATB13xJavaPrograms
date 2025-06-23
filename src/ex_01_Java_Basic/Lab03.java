e551954f1cd663cf24903f682665eee9dd7fd559
MAM11 <mwa.mnk@gmail.com>
2025-06-23 18:04:07 +0530

    10th June

--- src/ex_01_Java_Basic/Lab03.java
+++ src/ex_01_Java_Basic/Lab03.java
@@ -1,4 +1,10 @@
 package ex_01_Java_Basic;
 
 public class Lab03 {
+    public static void main(String[] args) {
+        int b=9;
+        for (int i=1; i<=10; i++){
+            System.out.printf("%dx%d=%d\n",b,i,b*i);
+        }
+    }
 }
---
+++ src/ex_02_Java_Basic/VariablesandLiterals.java
@@ -0,0 +1,8 @@
+package ex_02_Java_Basic;
+
+public class VariablesandLiterals {
+    static int a=1;
+    public static void main(String[] args) {
+        System.out.println(a);
+    }
+}
