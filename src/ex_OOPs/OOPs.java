eb8161ade42cb0440407b0caf61ec4c6050a6316
MAM11 <mwa.mnk@gmail.com>
2025-07-19 12:53:10 +0530

    19th July: OOPs

---
+++ src/ex_OOPs/OOPs.java
@@ -0,0 +1,21 @@
+package ex_OOPs;
+
+public class OOPs {
+    public static void main(String[] args){
+        Pen p1=new Pen();
+        p1.setColor("Blue");
+        p1.setTip(5);
+        System.out.println(p1.color+"  "+p1.tip);
+    }
+}
+
+class Pen{
+    String color;
+    int tip;
+    void setColor(String newColor){
+        color = newColor;
+    }
+    void setTip(int newTip){
+        tip = newTip;
+    }
+}
